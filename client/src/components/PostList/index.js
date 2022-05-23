e9e1a412cdc8aa56dc550fee427f1422f30bee01
brith <bgc@briths-MacBook-Air.local>
2022-05-22 20:02:49 -0700

    Start with styling home.

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -12,43 +12,74 @@ const PostList = ({
   }
 
   return (
-    <div>
+    <div style = {styles.thePost}>
       {showTitle && <h3>{title}</h3>}
       {posts &&
         posts.map((post) => (
-          <div key={post._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
+          <div key={post._id} >
+            <h4 style={styles.textA}>
               {showUsername ? (
                 <Link
-                  className="text-light"
+                  style={styles.textLight}
                   to={`/dashboard/${post.postTitle}`}
                 >
-                  {post.postTitle} <br />
-                  <span style={{ fontSize: '1rem' }}>
-                    had this post on {post.createdAt}
+                  {post.postTitle}
+                  <span style={styles.pdate}>
+                    posted by {post.createdAt}
                   </span>
                 </Link>
               ) : (
                 <>
-                  <span style={{ fontSize: '1rem' }}>
-                    You had this post on {post.createdAt}
-                  </span>
+                  {/* <span style={{ fontSize: '1rem' }}>
+                    posted by {post.createdAt}
+                  </span> */}
                 </>
               )}
             </h4>
             <div className="card-body bg-light p-2">
-              <p>{post.postTitle}</p>
+              <p style={styles.p}>{post.postDescription}</p>
             </div>
             <Link
               className="btn btn-primary btn-block btn-squared"
               to={`/POSTS/${post._id}`}
             >
-              Join the discussion on this post.
             </Link>
           </div>
         ))}
     </div>
   );
 };
+
+const styles = {
+
+  thePost: {
+    fontSize: '25px',
+
+  },
+  textLight: {
+    color: 'beige',
+    textDecoration: 'none',
+    marginLeft: '10px',
+    marginRigth: '10px'
+
+  },
+  textA: {
+    backgroundColor: 'rgb(132, 87, 4)',
+    height: '70px',
+    border: 'solid',
+    marginBottom: '-10px',
+    borderColor: 'rgb(33, 54, 33)'
+  },
+  pdate: {
+    fontSize: '15px',
+
+    marginLeft: '30px'
+  },
+  p: {
+    fontSize: '20px',
+    marginLeft: '10px',
+    marginRigth: '10px'
+  }
+}
 
 export default PostList;
