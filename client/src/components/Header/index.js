b0b2de281dcf96d05365b3edfe66a2c68939f321
brith <bgc@briths-MacBook-Air.local>
2022-05-22 17:12:06 -0700

    Few styling features for header.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,36 +3,70 @@ import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
 
+const styles = {
+  header: {
+    backgroundColor: 'orange',
+  },
+  hdr: {
+    boxShadow: '5px 5px 7px rgb(151, 151, 143',
+    height: '130px',
+    paddingLeft: '10px',
+    paddingTop: '0.01px'
+
+  },
+  titlebox: {
+    // paddingBottom: 'px'
+  },
+  title: {
+    textDecoration: 'none',
+    color: 'beige',
+    
+  },
+  navLinks: {
+    color: 'beige',
+    float: 'right',
+    marginRight: '15px'
+  },
+  login: {
+    textDecoration: 'none',
+    color: 'beige',
+  },
+  sigup: {
+    textDecoration: 'none',
+    color: 'beige',
+  },
+}
+
 const Header = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">Anime Alter</h1>
+    <header style={styles.header}>
+      <div style={styles.hdr}>
+        <div style={styles.titlebox}>
+          <Link style={styles.title} to="/">
+            <h1 >Anime Alter</h1>
           </Link>
-          <p className="m-0"></p>
+          <p></p>
         </div>
-        <div>
+        <div style={styles.navLinks}>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
+              <Link to="/me">
                 {Auth.getDashboard().data.username}'s dashboard
               </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+              <button onClick={logout}>
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
+              <Link style={styles.login} to="/login">
                 Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
+              </Link>/
+              <Link style={styles.sigup} to="/signup">
                 Signup
               </Link>
             </>
